ea6234052a7b829f9eeb3f6e72339c6cfb6db76c
Kartik K <krkhengate123@gmail.com>
2024-10-14 23:20:57 +0530

    services created

---
+++ src/main/java/com/example/lms/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.lms.service.impl;
+
+import com.example.lms.entity.Employee;
+import com.example.lms.repo.EmployeeRepo;
+import com.example.lms.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+   private EmployeeRepo employeeRepo;
+    @Override
+    public Employee saveemployee(Employee employee) {
+
+        Employee save = employeeRepo.save(employee);
+
+        return save;
+    }
+
+    @Override
+    public String deleteEMployee(Integer empId) {
+
+        employeeRepo.deleteById(empId);
+
+        return "Employee Deleted";
+    }
+
+    @Override
+    public Employee getEmployeeById(Integer empId) {
+        Employee byId = employeeRepo.findById(empId).get();
+        return byId;
+    }
+
+    @Override
+    public List<Employee> getAllEmployee() {
+
+        List<Employee> all = employeeRepo.findAll();
+        return all;
+    }
+}
