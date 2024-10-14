bac794d66bc1b0b528aeeb698a12e4e0b41c3867
Kartik K <krkhengate123@gmail.com>
2024-10-14 22:31:09 +0530

    syso added

--- src/main/java/com/example/lms/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/lms/LibraryManagementSystemApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryManagementSystemApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Welcome to Java");
+
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
 	}
 
